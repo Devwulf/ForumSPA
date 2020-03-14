cecd9dd172c3c8bd46b7793a030736422baf9ee0
Devwulf <mark.malabanan24@gmail.com>
2020-03-13 23:06:18 -0400

    Added Seed Data

---
+++ Server/Data/SeedData.cs
@@ -0,0 +1,87 @@
+﻿using ForumSPA.Server.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ForumSPA.Server.Data
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new ApplicationDbContext(
+                serviceProvider.GetRequiredService<DbContextOptions<ApplicationDbContext>>()))
+            {
+                // If true, hubs is already seeded
+                if (context.Hubs.Any())
+                    return;
+
+                context.Hubs.AddRange(
+                    new Hub()
+                    {
+                        Name = "General"
+                    },
+                    new Hub()
+                    {
+                        Name = "Fight Club",
+                        Description = "Fight!",
+                        Rules = "Rule 1: You do not talk about Fight Club."
+                    });
+
+                context.SaveChanges();
+
+                if (context.Threads.Any())
+                    return;
+
+                context.Threads.AddRange(
+                    new Thread()
+                    {
+                        Name = "Hello World!",
+                        HubId = 1
+                    },
+                    new Thread()
+                    {
+                        Name = "Goodbye World!",
+                        HubId = 1
+                    });
+
+                context.SaveChanges();
+
+                if (context.Posts.Any())
+                    return;
+
+                context.Posts.AddRange(
+                    new Post()
+                    {
+                        Body = "How are you?",
+                        ThreadId = 1,
+                        IsMainPost = true
+                    },
+                    new Post()
+                    {
+                        Body = "I'm fine, thank you!",
+                        ThreadId = 1,
+                        IsMainPost = false
+                    },
+                    new Post()
+                    {
+                        Body = "Have a great day!",
+                        ThreadId = 2,
+                        IsMainPost = true
+                    },
+                    new Post()
+                    {
+                        Body = "See you later!",
+                        ThreadId = 2,
+                        IsMainPost = false
+                    });
+
+                context.SaveChanges();
+
+            }
+        }
+    }
+}
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.AspNetCore;
+﻿using ForumSPA.Server.Data;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
 
 namespace ForumSPA.Server
 {
@@ -8,7 +12,24 @@ namespace ForumSPA.Server
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            var host = BuildWebHost(args);
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    SeedData.Initialize(services);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occured seeding the DB.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
